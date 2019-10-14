135f1bc5f5d0be714548f5a60d3c1b677f86af2c
aidansummerville <g00302722@gmit.ie>
2019-10-14 16:04:45 +0100

    shop

---
+++ myShop.c
@@ -0,0 +1,16 @@
+//Build a Simulation of a shop in c
+
+#include <stdio.h>
+#include<string.h>
+
+struct Customer{
+    char* name;
+    double budget;
+};
+
+
+
+int main(void)
+{
+    return 0;
+}
