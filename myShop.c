c872331e1b37ebfa843e7f5d6c295893062f85e7
aidansummerville <g00302722@gmit.ie>
2019-10-14 16:57:49 +0100

    shop structs

--- myShop.c
+++ myShop.c
@@ -3,14 +3,52 @@
 #include <stdio.h>
 #include<string.h>
 
+
+
+struct Product{
+    char* name;
+    double price;
+
+};
+struct ProductStock{
+    struct Product product;
+    int quantity;
+};
+
 struct Customer{
     char* name;
     double budget;
+    struct ProductStock shoppingList[10];
 };
 
 
+struct Shop{
+    double cash;
+    struct ProductStock stock[20];
+};
+
+void printProduct(struct Product p){
+    printf("------------------\n");
+    printf("PRODUCT NAME: %s \nPRODUCT PRICE: %.2f\n", p.name, p.price);
+    printf("------------------\n");
+}
+
+void printCustomer(struct Customer c){
+    printf("------------------\n");
+    printf("CUSTOMER NAME: %s \nCUSTOMER BUDGET: %.2f\n", c.name, c.budget);
+    printf("------------------\n");
+}
 
 int main(void)
 {
+    struct Customer aidan = { "Aidan", 100.0};
+    printCustomer(aidan);
+
+    struct Product coke  = { "Can Coke", 1.10};
+    printProduct(coke);
+    
+    struct ProductStock cokeStock = {coke, 20};
+    printf("The shop has %d of the product %s\n",cokeStock.quantity, cokeStock.product.name);
+
     return 0;
 }
